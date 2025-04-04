3fb200ca741f715d86745ec0e9ef1128e21dce9e
DevOps-Youtube-Channel <98235444+DevOps-Youtube-Channel@users.noreply.github.com>
2025-04-04 11:08:58 +0500

    Update master_install.sh

--- devops-tools/kubernetes/kubeadm/bash_script/master_install.sh
+++ devops-tools/kubernetes/kubeadm/bash_script/master_install.sh
@@ -95,13 +95,13 @@ echo -e "${YELLOW}--------------------------------------------------------------
 echo -e "${LIGHT_GREEN}Installing Docker...${NC}"
 echo -e "${LIGHT_BLUE}"
 sudo apt-get update -y
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
