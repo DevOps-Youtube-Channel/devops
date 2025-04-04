bd1ba2c1a4abc1fea8ba5465ef971f5a946087f2
DevOps-Youtube-Channel <98235444+DevOps-Youtube-Channel@users.noreply.github.com>
2025-04-04 14:47:45 +0500

    Update master.sh

--- devops-tools/kubernetes/kubeadm/bash_script/master.sh
+++ devops-tools/kubernetes/kubeadm/bash_script/master.sh
@@ -52,9 +52,11 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gpg gnupg2 soft
 
 7) Установим тулы Кубернетес
 sudo mkdir -m 755 /etc/apt/keyrings
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt update
+sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 lsmod | grep "overlay\|br_netfilter"
